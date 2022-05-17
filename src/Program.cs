c0d8ff42e713072d7e4945333ca5c52a1a3a36b8
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2022-05-17 07:06:20 -0400

    update to dotnet 6 and v1.8

--- src/Program.cs
+++ src/Program.cs
@@ -20,6 +20,35 @@ namespace openrmf_msg_template
 
             // setup the NLog name
             var logger = LogManager.GetLogger("openrmf_msg_template");
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("LOGLEVEL"))) // default
+                LogManager.Configuration.Variables["logLevel"] = "Warn";
+            else if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("LOGLEVEL"))) {
+                switch (Environment.GetEnvironmentVariable("LOGLEVEL"))
+                {
+                    case "5":
+                        LogManager.Configuration.Variables["logLevel"] = "Critical";
+                        break;
+                    case "4":
+                        LogManager.Configuration.Variables["logLevel"] = "Error";
+                        break;
+                    case "3":
+                        LogManager.Configuration.Variables["logLevel"] = "Warn";
+                        break;
+                    case "2":
+                        LogManager.Configuration.Variables["logLevel"] = "Info";
+                        break;
+                    case "1":
+                        LogManager.Configuration.Variables["logLevel"] = "Debug";
+                        break;
+                    case "0":
+                        LogManager.Configuration.Variables["logLevel"] = "Trace";
+                        break;
+                    default:
+                        LogManager.Configuration.Variables["logLevel"] = "Warn";
+                        break;
+                }
+            }
+            LogManager.ReconfigExistingLoggers();
 
             // Create a new connection factory to create a connection.
             ConnectionFactory cf = new ConnectionFactory();
