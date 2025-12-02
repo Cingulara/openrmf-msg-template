4107d703cf3558769f8369f840ad55a836438f89
Dale Bingham <dale.bingham@soteriasoft.com>
2025-12-02 07:00:08 -0500

    updated filter on template names

--- src/Program.cs
+++ src/Program.cs
@@ -159,18 +159,19 @@ namespace openrmf_msg_template
                 // remove the NIWC Enhanced type of ending
                 title = title.Substring(0, title.IndexOf("- NIWC")).Trim();
             }
-            return title.Replace("STIG", "Security Technical Implementation Guide").Replace("MS Windows","Windows")
-                .Replace("SCAP Benchmark","").Replace(" SCAP","").Replace("Cisco IOS-XE","Cisco IOS XE").Replace("Cisco NX-OS", "Cisco NX OS")
-                .Replace("Cisco IOS-XR","Cisco IOS XR")
-                .Replace("Microsoft Windows","Windows").Replace("Dot Net","DotNet").Replace("Microsoft Windows Defender", "Microsoft Defender")
-                .Replace("Windows Defender", "Microsoft Defender").Replace("Windows Server 2012 MS", "Windows Server 2012/2012 R2 Member Server")
-                .Replace("Windows Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
-                .Replace("Microsoft Windows Defender Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
-                .Replace("Microsoft Defender Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
-                .Replace("Mozilla Firefox for Windows", "Mozilla Firefox").Replace("Mozilla Firefox for Linux", "Mozilla Firefox")
-                .Replace("Mozilla Firefox for Unix", "Mozilla Firefox").Replace("IIS 10.0 Web Server","IIS 10.0 Server")
-                .Replace("IIS 10.0 Web Site","IIS 10.0 Site")
-                .Trim();
+            return title.Replace("STIG", "Security Technical Implementation Guide").Replace("MS Windows", "Windows")
+            .Replace("SCAP Benchmark", "").Replace(" SCAP", "").Replace("Cisco IOS-XE", "Cisco IOS XE").Replace("Cisco NX-OS", "Cisco NX OS")
+            .Replace("Cisco IOS-XR", "Cisco IOS XR")
+            .Replace("Microsoft Windows", "Windows").Replace("Dot Net", "DotNet").Replace("Microsoft Windows Defender", "Microsoft Defender")
+            .Replace("Windows Defender", "Microsoft Defender").Replace("Windows Server 2012 MS", "Windows Server 2012/2012 R2 Member Server")
+            .Replace("Windows Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
+            .Replace("Microsoft Windows Defender Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
+            .Replace("Microsoft Defender Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
+            .Replace("Windows Firewall", "Windows Defender Firewall with Advanced Security")
+            .Replace("Mozilla Firefox for Windows", "Mozilla Firefox").Replace("Mozilla Firefox for Linux", "Mozilla Firefox")
+            .Replace("Mozilla Firefox for Unix", "Mozilla Firefox").Replace("IIS 10.0 Web Server", "IIS 10.0 Server")
+            .Replace("IIS 10.0 Web Site", "IIS 10.0 Site").Replace("MS SQL Server", "Microsoft SQL Server")
+            .Trim();
         }
 
         /// <summary>
